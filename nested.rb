39ca48551403a9cb852c1ba0b2ca09873d21d34b
Samantha brake <sammy.brake@gmail.com>
2018-09-11 19:57:09 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     new_string = ""
     new_string =  programmer_hash[:dennis_ritchie][:languages]
     return new_string
 end
